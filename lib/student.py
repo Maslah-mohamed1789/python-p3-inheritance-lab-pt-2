d779961d288ed5e12a04d80f70f7106cd67876b2
maslah <Maslah-mohamed1789>
2024-12-02 15:28:16 +0300

    completed the lab

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,36 @@
+# Define the Student class
 class Student:
-    pass
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
 
+    def raise_hand(self):
+        print("Pick me!")
+
+# Define the ChattyStudent class that inherits from Student
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        # Call the parent class's hello() method
+        super().hello()
+        # Add the chatty student's additional dialogue
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch "
+              "The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, "
+              "you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        # Use super() to call the parent class's raise_hand() method 10 times
+        for _ in range(10):
+            super().raise_hand()
+
+# Testing the functionality
+if __name__ == "__main__":
+    # Test the Student class
+    student = Student()
+    print("Student says:")
+    student.hello()
+    student.raise_hand()
+
+    print("\nChattyStudent says:")
+    # Test the ChattyStudent class
+    chatty_student = ChattyStudent()
+    chatty_student.hello()
+    chatty_student.raise_hand()
